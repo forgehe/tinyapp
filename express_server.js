5a2f01f9b0432095eb8dc59d10cc754e2ce578e3
Richard Liao <liao098@gmail.com>
2020-02-17 22:32:33 +0000

    setup ejs and views folder

--- express_server.js
+++ express_server.js
@@ -7,12 +7,34 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
+app.set("view engine", "ejs");
+
+// index page
+app.get("/", function(req, res) {
+  let drinks = [
+    { name: "Bloody Mary", drunkness: 3 },
+    { name: "Martini", drunkness: 5 },
+    { name: "Scotch", drunkness: 10 }
+  ];
+  let tagline =
+    "Any code of your own that you haven't looked at for six or more months might as well have been written by someone else.";
+
+  res.render("pages/index", {
+    drinks: drinks,
+    tagline: tagline
+  });
+});
+
+// about page
+app.get("/about", function(req, res) {
+  res.render("pages/about");
 });
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.listen(PORT, () => {
